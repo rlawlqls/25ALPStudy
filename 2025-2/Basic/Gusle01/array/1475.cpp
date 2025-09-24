d8fc63279178fe99e5cf08b61f512a7f122face7
Gusle01 <qkrdudwn010@naver.com>
2025-09-24 12:13:15 +0900

    Create 1475.cpp

---
+++ 2025-2/Basic/Gusle01/array/1475.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, arr[10] = {0, }, temp, max = -1;
+    cin >> n;
+    
+    string str_n = to_string(n);
+    for(int i = 0; i < str_n.length(); i++){
+        arr[str_n[i] - '0']++;
+    }
+
+    for(int i = 0; i < 10; i++){
+        if(i == 6 || i == 9) continue;
+        if(arr[i] > max){
+            max  = arr[i];
+        }
+    }
+    
+    temp = arr[6] + arr[9];
+    temp = (temp + 1) / 2;
+    if(temp > max){
+        max = temp;
+    }
+    
+    cout << max;
+    return 0;
+}
